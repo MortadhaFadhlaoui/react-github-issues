ed9d701767fa26973862f2cdedc13a58052918b4
mortadha fadhlaoui <mortadhafadhlaoui@gmail.com>
2023-01-21 17:14:50 +0100

    initialize the project Add esLint Ass prettier Add Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+	entry: './src/index.tsx',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader' },
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(gif|svg|jpg|png)$/,
+				loader: 'file-loader',
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	mode: 'development',
+}
