9b60e443a6bbb8dd510651a7087384549b7b8cc6
mortadha fadhlaoui <mortadhafadhlaoui@gmail.com>
2023-01-26 16:21:28 +0100

    move api url to constants file

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { REACT_APP_GITHUB_API_URL } from '../utils/constants'
 
 export const client = new ApolloClient({
-	uri: process.env.REACT_APP_GITHUB_API_URL,
+	uri: REACT_APP_GITHUB_API_URL,
 	headers: {
 		authorization: `Token ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
 	},
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,4 @@ export const REPO_OWNER = 'facebook'
 export const REPO_NAME = 'react'
 export const STATUS = [IssueState.OPEN, IssueState.CLOSED]
 export const PAGE_NUMBER = 10
+export const REACT_APP_GITHUB_API_URL = 'https://api.github.com/graphql'
